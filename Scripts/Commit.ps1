f62a01669017b987185106b08ab6369e5b11e7e0
Travis Coleman <tcoleman@allyis.com>
2016-08-05 12:19:30 -0700

    Modifying commit to check for changes

--- Scripts/Commit.ps1
+++ Scripts/Commit.ps1
@@ -1,5 +1,11 @@
 $curDate = Get-Date
-svn commit -m "$curDate"
-git tag "Release - $curDate"
-git push --tags
+$curCommit = git log -n1 --oneline
+$lastCommit = type ..\..\curCommit.log
+if($curCommit -ne $lastCommit)
+{
+    $curCommit > ..\..\curCommit.log
+    svn commit -m "$curDate"
+    git tag "Release - $curDate"
+    git push --tags
+}
 exit 1
