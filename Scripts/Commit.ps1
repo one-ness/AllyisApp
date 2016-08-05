a8e350ad28ab07e585a99f876dca1cd3b26d71da
Travis Coleman <tcoleman@allyis.com>
2016-08-05 15:15:16 -0700

    Changing tags to use folder structure

--- Scripts/Commit.ps1
+++ Scripts/Commit.ps1
@@ -5,7 +5,7 @@ if($curCommit -ne $lastCommit)
 {
     $curCommit > ..\..\curCommit.log
     svn commit -m "$curDate"
-    git tag "Release_$curDate"
+    git tag "Release/$curDate"
     git push --tags
 }
 exit 1
