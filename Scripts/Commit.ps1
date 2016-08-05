ed7dc197801fd8ebf241c7fd07c5e3685d5a3c25
Travis Coleman <tcoleman@allyis.com>
2016-08-05 14:56:53 -0700

    Adding Date format to tag

--- Scripts/Commit.ps1
+++ Scripts/Commit.ps1
@@ -1,11 +1,11 @@
-$curDate = Get-Date
+$curDate = Get-Date -Format "MM-dd-yyyy_HH.mm.ss"
 $curCommit = git log -n1 --oneline
 $lastCommit = type ..\..\curCommit.log
 if($curCommit -ne $lastCommit)
 {
     $curCommit > ..\..\curCommit.log
     svn commit -m "$curDate"
-    git tag "Release - $curDate"
+    git tag "Release_$curDate"
     git push --tags
 }
 exit 1
