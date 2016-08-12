33c392d6d26d2a8b43ea189fe080943a9ac349a6
Travis Coleman <tcoleman@allyis.com>
2016-08-12 11:27:04 -0700

    Add build and deployment to the scripts

--- Scripts/Build.ps1
+++ Scripts/Build.ps1
@@ -1,1 +1,11 @@
-exit 1
+msbuild /m /nologo /noconlog .\src\Main\AllyisApps.sln /t:Clean
+$result = msbuild /m /nologo .\src\Main\AllyisApps.sln /t:Build
+foreach($s in $result) {if($s.Contains("FAILED")) {$res+=$s}}
+if($res -eq "")
+{
+	exit 1
+}
+else
+{
+	exit 0
+}
--- Scripts/Deploy.ps1
+++ Scripts/Deploy.ps1
@@ -1,1 +1,17 @@
+$srcPath = ".\src\Main\aaweb\"
+$destPath = "c:\aa\"
+
+Copy-Item -Path ($srcPath + "App_Data\") -Destination $destPath -Recurse
+Copy-Item -Path ($srcPath + "Areas\") -Destination $destPath -Recurse
+Copy-Item -Path ($srcPath + "bin\") -Destination $destPath -Recurse
+Copy-Item -Path ($srcPath + "Content\") -Destination $destPath -Recurse
+Copy-Item -Path ($srcPath + "fonts\") -Destination $destPath -Recurse
+Copy-Item -Path ($srcPath + "Scripts\") -Destination $destPath -Recurse
+Copy-Item -Path ($srcPath + "Views\") -Destination $destPath -Recurse
+Copy-Item -Path ($srcPath + "favicon.ico") -Destination $destPath -Recurse
+Copy-Item -Path ($srcPath + "Global.asax") -Destination $destPath -Recurse
+Copy-Item -Path ($srcPath + "packages.config") -Destination $destPath -Recurse
+Copy-Item -Path ($srcPath + "Parameters.xml") -Destination $destPath -Recurse
+Copy-Item -Path ($srcPath + "Web.config") -Destination $destPath -Recurse
+
 exit 1
