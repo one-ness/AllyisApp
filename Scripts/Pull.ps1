4504896ebff47b420843be58b8d07600c63195e9
Travis Coleman <tcoleman@allyis.com>
2016-08-15 12:17:52 -0700

    Fixing pull script

--- Scripts/Pull.ps1
+++ Scripts/Pull.ps1
@@ -1,4 +1,4 @@
 git fetch
-foreach ($t in dir -Directory | Select-Object Name) {if($t.Name -ne "Scripts") { del -Recurse $t.Name}}
+foreach ($t in dir -Directory | Select-Object Name) {if($t.Name -ne "Scripts") { del -Force -Recurse $t.Name}}
 git reset --hard origin/master
 exit 1
