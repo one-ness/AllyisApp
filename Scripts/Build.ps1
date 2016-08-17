d0a3e80d24b28d60f86293c4db5cbb735b25d123
Travis Coleman <tcoleman@allyis.com>
2016-08-17 12:29:02 -0700

    Adding nuget restore to build script

--- Scripts/Build.ps1
+++ Scripts/Build.ps1
@@ -1,5 +1,6 @@
-&"C:\Program Files (x86)\MSBuild\14.0\Bin\MSBuild.exe" /m /nologo /noconlog .\src\Main\aaweb\AllyisApps.csproj /t:Clean /p:Configuration=Release
-$result = &"C:\Program Files (x86)\MSBuild\14.0\Bin\MSBuild.exe" /m /nologo .\src\Main\aaweb\AllyisApps.csproj /t:Build /p:Configuration=Release
+&"C:\Program Files (x86)\MSBuild\14.0\Bin\MSBuild.exe" /m /nologo /noconlog .\src\Main\AllyisApps.sln /t:Clean /p:Configuration=Release
+&"F:\WorkingDir\nuget.exe" restore .\src\Main\AllyisApps.sln
+$result = &"C:\Program Files (x86)\MSBuild\14.0\Bin\MSBuild.exe" /m /nologo .\src\Main\AllyisApps.sln /t:Build /p:Configuration=Release
 Write-Host $result
 foreach($s in $result) {if($s.Contains("FAILED")) {$res+=$s}}
 if($res -eq "")
