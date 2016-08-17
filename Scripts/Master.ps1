6b5bad86c9b6407fd326d8d98616d62b271697d2
Travis Coleman <tcoleman@allyis.com>
2016-08-17 10:24:37 -0700

    Hard-coding path to msbuild and modifying logging on scripts

--- Scripts/Master.ps1
+++ Scripts/Master.ps1
@@ -1,10 +1,10 @@
 Start-Transcript -Append -Path ..\ExecLog.log -IncludeInvocationHeader
 PowerShell -NonInteractive -NoProfile -Command {Scripts\Pull.ps1; exit $LASTEXITCODE}
-if($LASTEXITCODE -ne 1) { exit "Failed on Pull" } else { Write-Host "Pull Succeeded"}
+if($LASTEXITCODE -ne 1) { Write-Host "Failed on Pull"; exit } else { Write-Host "Pull Succeeded"}
 PowerShell -NonInteractive -NoProfile -Command {Scripts\Build.ps1; exit $LASTEXITCODE}
-if($LASTEXITCODE -NE 1) { exit "Failed on Build" } else { Write-Host "Build Succeeded"}
+if($LASTEXITCODE -NE 1) { Write-Host "Failed on Build"; exit } else { Write-Host "Build Succeeded"}
 PowerShell -NonInteractive -NoProfile -Command {Scripts\Commit.ps1; exit $LASTEXITCODE}
-if($LASTEXITCODE -NE 1) { exit "Failed on Commit" } else { Write-Host "Commit Succeeded"}
+if($LASTEXITCODE -NE 1) { Write-Host "Failed on Commit"; exit } else { Write-Host "Commit Succeeded"}
 PowerShell -NonInteractive -NoProfile -Command {Scripts\Deploy.ps1; exit $LASTEXITCODE}
-if($LASTEXITCODE -NE 1) { exit "Failed on Deploy" } else { Write-Host "Deploy Succeeded"}
+if($LASTEXITCODE -NE 1) { Write-Host "Failed on Deploy"; exit } else { Write-Host "Deploy Succeeded"}
 Stop-Transcript
