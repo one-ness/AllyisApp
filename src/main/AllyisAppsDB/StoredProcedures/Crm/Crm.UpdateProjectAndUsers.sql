87db5f834a69cb8fc880f392af66a2cce18f1578
James Porter <porterjamesf@gmail.com>
2016-08-22 08:46:37 -0700

    Edits Crm.UpdateProjectAndUsers to use existing table type and dynamic parameters.

--- src/main/AllyisAppsDB/StoredProcedures/Crm/Crm.UpdateProjectAndUsers.sql
+++ src/main/AllyisAppsDB/StoredProcedures/Crm/Crm.UpdateProjectAndUsers.sql
@@ -1,14 +1,10 @@
-﻿CREATE TYPE UserIDTable AS TABLE
-( UserID INT );
-GO
-
-CREATE PROCEDURE [Crm].[UpdateProjectAndUsers]
+﻿CREATE PROCEDURE [Crm].[UpdateProjectAndUsers]
 	@ProjectId INT,
 	@Name NVARCHAR(MAX),
 	@PriceType NVARCHAR(20),
     @StartingDate DATE,
     @EndingDate DATE,
-	@UserIDs UserIDTable READONLY
+	@UserIDs [Auth].[UserTable] READONLY
 AS
 BEGIN
 	SET NOCOUNT ON;
@@ -17,14 +13,14 @@ BEGIN
 		/* Update new users that used to be users at some point */
 		UPDATE [Crm].[ProjectUser] SET IsActive = 1
 		WHERE [ProjectUser].[ProjectId] = @ProjectId 
-			AND [ProjectUser].[UserId] IN (SELECT UserID FROM @UserIDs) 
+			AND [ProjectUser].[UserId] IN (SELECT userId FROM @UserIDs) 
 			AND [ProjectUser].[IsActive] = 0
 
 		/* Add new users that have never been on the project */
 		INSERT INTO [Crm].[ProjectUser] ([ProjectId], [UserId], [IsActive])
-		SELECT @ProjectId, UserID, 1
+		SELECT @ProjectId, userId, 1
 		FROM @UserIDs
-		WHERE UserID NOT IN
+		WHERE userId NOT IN
 			(SELECT [ProjectUser].[UserId]
 			FROM [Crm].[ProjectUser]
 			WHERE [ProjectUser].[ProjectId] = @ProjectId)
@@ -32,7 +28,7 @@ BEGIN
 		/* Set inactive existing users that are not in the updated users list */
 		UPDATE [Crm].[ProjectUser] SET IsActive = 0
 		WHERE [ProjectUser].[ProjectId] = @ProjectId
-			AND [ProjectUser].[UserId] NOT IN (SELECT UserID FROM @UserIDs) 
+			AND [ProjectUser].[UserId] NOT IN (SELECT userId FROM @UserIDs) 
 			AND [ProjectUser].[IsActive] = 1
 
 		/* Update other project properties */
