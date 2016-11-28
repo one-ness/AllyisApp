d9960256dc1e423c962f57b02e7eba9cd00c527a
Trenton Sanders <trentons@allyis.com>
2016-11-28 14:23:50 -0800

    Groundwork for Import functionality. OleDB setup done for ImportCustomers and ImportProject methods, and a constants file for service constants (like excel column names).

---
+++ src/main/AllyisApps.Services/ServiceConstants.cs
@@ -0,0 +1,16 @@
+﻿namespace AllyisApps.Services
+{
+    /// <summary>
+    /// Constants for file column header strings (like excel), to be used in imports and exports 
+    /// </summary>
+    class FileHeaders
+    {
+        #region customer
+        /// <summary>
+        /// Customer-Property: Name
+        /// </summary>
+        public const string CustomerName = "Customer Name";
+
+        #endregion customer
+    }
+}
