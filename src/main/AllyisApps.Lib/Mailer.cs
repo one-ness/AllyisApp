e9e3688f2db01e13814f93a9f35c8191866c8845
James Porter <jporter@allyis.com>
2016-11-29 11:43:37 -0800

    Some groundwork for SendGrid

--- src/main/AllyisApps.Lib/Mailer.cs
+++ src/main/AllyisApps.Lib/Mailer.cs
@@ -4,7 +4,11 @@
 // </copyright>
 //------------------------------------------------------------------------------
 
+using System;
 using System.Threading.Tasks;
+
+using SendGrid;
+using SendGrid.Helpers.Mail;
 
 namespace AllyisApps.Lib
 {
@@ -13,56 +17,36 @@ namespace AllyisApps.Lib
 	/// </summary>
 	public static class Mailer
 	{
-		/// <summary>
-		/// Gets or sets the Server name.
-		/// </summary>
-		private static string ServerName { get; set; }
-
-		/// <summary>
-		/// Gets or sets the Port.
-		/// </summary>
-		private static int Port { get; set; }
-
-		/// <summary>
-		/// Gets or sets the username.
-		/// </summary>
-		private static string Username { get; set; }
-
-		/// <summary>
-		/// Gets or sets the password.
-		/// </summary>
-		private static string Password { get; set; }
-
-		/// <summary>
-		/// Init the mailer.
-		/// </summary>
-		/// <param name="serverName">Mail server name.</param>
-		/// <param name="port">Mailing port.</param>
-		/// <param name="username">Mailing username.</param>
-		/// <param name="password">Mailing password.</param>
-		public static void Init(string serverName, int port, string username, string password)
-		{
-			ServerName = serverName;
-			Port = port;
-			Username = username;
-			Password = password;
-		}
+        static SendGridAPIClient sender
+        {
+            get
+            {
+                if (senderBack == null)
+                {
+                    senderBack = new SendGridAPIClient(Environment.GetEnvironmentVariable("SENDGRID_APIKEY"));
+                }
+                return senderBack;
+            }
+        }
+        private static SendGridAPIClient senderBack;
 
 		/// <summary>
 		/// Send email.
 		/// </summary>
 		/// <param name="from">Who the message is from.</param>
-		/// <param name="recipients">The message recipeient.</param>
+		/// <param name="to">The message recipient.</param>
 		/// <param name="subject">The message subject line.</param>
 		/// <param name="bodyHtml">The html body of the email.</param>
 		/// <returns>The async mailing task.</returns>
-		public static async Task SendEmailAsync(string from, string recipients, string subject, string bodyHtml)
+		public static async Task SendEmailAsync(string from, string to, string subject, string bodyHtml)
 		{
-			await Task.Run(() =>
-		   {
-			   // TODO: implement using SMTP library
-			   return;
-		   });
+            Mail mail = new Mail(
+                new Email(from),
+                subject,
+                new Email(to),
+                new Content("text/html", bodyHtml)
+                );
+            await sender.client.mail.send.post(requestBody: mail.Get());
 		}
 	}
 }
