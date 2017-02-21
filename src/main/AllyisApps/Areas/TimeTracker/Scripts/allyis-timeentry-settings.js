06e9ccfc032dc7ede4bed49d0a2520ae21af9c74
James Porter <jporter@allyis.com>
2017-02-21 15:24:40 -0800

    Start on confirmations/notifications for pay classes

--- src/main/AllyisApps/Areas/TimeTracker/Scripts/allyis-timeentry-settings.js
+++ src/main/AllyisApps/Areas/TimeTracker/Scripts/allyis-timeentry-settings.js
@@ -1,3 +1,10 @@
-﻿$(document).ready(function () {
-	alert("Script loaded");
+﻿//deletePayClass = function (payClasId) {
+//	console.log("delete pay class " + payClassId);
+//}
+
+$(document).ready(function () {
+	console.log("script running");
+	$('.deletePayClass').on('click', function () {
+		console.log("delete pay class " + payClassId);
+	});
 });
