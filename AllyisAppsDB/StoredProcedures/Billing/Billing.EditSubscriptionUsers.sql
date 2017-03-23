6b69f1a9d81cb4f8b6d5cc1b49207a424f155e1e
James Porter <jporter@allyis.com>
2017-03-23 13:31:17 -0700

    Edit subscription users sp for manage permissions consolidation

--- AllyisAppsDB/StoredProcedures/Billing/Billing.EditSubscriptionUsers.sql
+++ AllyisAppsDB/StoredProcedures/Billing/Billing.EditSubscriptionUsers.sql
@@ -18,20 +18,75 @@ BEGIN TRANSACTION
 	BEGIN
 		IF @TimeTrackerRole = -1 -- Removing users from subscription
 		BEGIN
+			DELETE [Billing].[SubscriptionUser] 
+			WHERE [SubscriptionId] = @SubId AND [UserId] IN (
+				SELECT [userId] FROM @UserIDs
+			)
 
+			SELECT @@ROWCOUNT
 		END
 		IF @TimeTrackerRole > 0 -- Adding/updating subscription users
 		BEGIN
-		-- TODO: Check for users being added vs. users just changing roles
-			DECLARE @ExistingUsers INT
+			-- Update users already in subscription that are changing roles
+			UPDATE [Billing].[SubscriptionUser] 
+			SET [ProductRoleId] = @TimeTrackerRole
+			WHERE [SubscriptionId] = @SubId AND [UserId] IN (
+				SELECT [userId] FROM @UserIDs
+			)
 
+			SELECT @@ROWCOUNT
+
+			-- Get number of users currently in subscription
+			DECLARE @ExistingUsers INT
 			SELECT @ExistingUsers = COUNT(*)
 			FROM [Billing].[SubscriptionUser] WITH (NOLOCK)
 			WHERE [SubscriptionId] = @SubId
+
+			-- Get users being added to subscription
+			DECLARE @AddingUsers TABLE (
+				[UserId] INT
+			);
+			INSERT INTO @AddingUsers ([UserId])
+			SELECT @UserIds.[userId]
+			FROM @UserIDs
+			LEFT OUTER JOIN (
+				SELECT [SubscriptionId], [UserId]
+				FROM [Billing].[SubscriptionUser] WITH (NOLOCK)
+				WHERE [SubscriptionId] = @SubId
+			) [SubUsers] ON [SubUsers].[UserId] = @UserIDs.[userId]
+			WHERE [SubscriptionId] IS NULL
+
+			-- Check that there is room for users being added
+			IF @ExistingUsers + (SELECT COUNT(*) FROM @AddingUsers) > @NumberOfUsers
+			BEGIN
+				SELECT -2 -- Not enough room in subscription
+			END
+			ELSE
+			BEGIN
+				-- Add the new users
+				DECLARE @SubAndRole TABLE ([SubId] INT, [TTRoleId] INT);
+				INSERT INTO @SubAndRole ([SubId], [TTRoleId]) VALUES (@SubId, @TimeTrackerRole)
+
+				INSERT INTO [Billing].[SubscriptionUser] ([SubscriptionId], [UserId], [ProductRoleId])
+				SELECT [SubId], [userId], [TTRoleId]
+				FROM @AddingUsers
+				CROSS JOIN @SubAndRole
+
+				SELECT COUNT(*) FROM @AddingUsers
+			END
+
+			--DECLARE @ChangingRoles TABLE (
+			--	[UserId] INT
+			--);
+			--INSERT INTO @ChangingRoles ([UserId])
+			--SELECT @UserIds.[userId]
+			--FROM [Billing].[SubscriptionUser] WITH (NOLOCK)
+			--INNER JOIN @UserIDs ON [SubscriptionUser].UserId = @UserIDs.[userId]
+			--WHERE [SubscriptionId] = @SubId AND [ProductRoleId] != @TimeTrackerRole
 		END
 	END
 	ELSE
-	BEGIN	-- No subscription to time tracker
-		SELECT -1
+	BEGIN	
+		SELECT -1 -- No subscription to time tracker
 	END
 COMMIT
