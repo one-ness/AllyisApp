8b59b652541a40345e51f4c898c56e33e9b5650a
Jake Marquez <jacob.marquez@allyis.com>
2017-03-24 12:53:05 -0700

    Added UpdateOrgInvitationEmployeeId store procedure

---
+++ AllyisAppsDB/StoredProcedures/Auth/Auth.UpdateOrgInvitationEmployeeId.sql
@@ -0,0 +1,7 @@
+﻿CREATE PROCEDURE [Auth].[UpdateOrgInvitationEmployeeId]
+	@InvitationId INT,
+	@OrganizationId INT,
+	@EmployeeId NVARCHAR(100)
+AS
+	SET NOCOUNT ON;
+	UPDATE [Auth].[Invitation] SET [EmployeeId] = @EmployeeId WHERE [InvitationId] = @InvitationId AND [OrganizationId] = @OrganizationId;
