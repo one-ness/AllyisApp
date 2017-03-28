43ec5067a30a13ea4107b772f224b6e119a6e9f7
Jake Marquez <jacob.marquez@allyis.com>
2017-03-28 12:59:58 -0700

    Updated AllyisAppsDB.sqlproj.rej and UpdateOrgUserEmployeeId stored procedure

--- AllyisAppsDB/StoredProcedures/Auth/Auth.UpdateOrgUserEmployeeId.sql
+++ AllyisAppsDB/StoredProcedures/Auth/Auth.UpdateOrgUserEmployeeId.sql
@@ -3,5 +3,19 @@
 	@OrganizationId INT,
 	@EmployeeId NVARCHAR(100)
 AS
+	IF EXISTS (
+			SELECT * FROM [Auth].[OrganizationUser] WITH (NOLOCK)
+			WHERE [OrganizationId] = @OrganizationId AND [EmployeeId] = @EmployeeId
+		) OR EXISTS (
+			SELECT * FROM [Auth].[Invitation] WITH (NOLOCK)
+			WHERE [OrganizationId] = @OrganizationId AND [IsActive] = 1 AND [EmployeeId] = @EmployeeId
+		)
+		BEGIN
+		SELECT 1;
+		END
+	ELSE
+	BEGIN
 	SET NOCOUNT ON;
 	UPDATE [Auth].[OrganizationUser] SET [EmployeeId] = @EmployeeId WHERE [UserId] = @UserId AND [OrganizationId] = @OrganizationId;
+	SELECT 2;
+	END
