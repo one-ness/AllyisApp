fb6829c67a3a4015c6b22ec68404b95514975c70
Jake Marquez <jacob.marquez@allyis.com>
2017-03-28 13:13:35 -0700

    Added UpdateOrgInvitationEmployeeId

--- AllyisAppsDB/StoredProcedures/Auth/Auth.UpdateOrgInvitationEmployeeId.sql
+++ AllyisAppsDB/StoredProcedures/Auth/Auth.UpdateOrgInvitationEmployeeId.sql
@@ -3,5 +3,19 @@
 	@OrganizationId INT,
 	@EmployeeId NVARCHAR(100)
 AS
+	IF EXISTS (
+			SELECT * FROM [Auth].[OrganizationUser] WITH (NOLOCK)
+			WHERE [OrganizationId] = @OrganizationId AND [EmployeeId] = @EmployeeId
+		) OR EXISTS (
+			SELECT * FROM [Auth].[Invitation] WITH (NOLOCK)
+			WHERE [OrganizationId] = @OrganizationId AND [IsActive] = 1 AND [EmployeeId] = @EmployeeId
+		)
+		BEGIN
+		SELECT 1;
+		END
+	ELSE
+	BEGIN
 	SET NOCOUNT ON;
-	UPDATE [Auth].[Invitation] SET [EmployeeId] = @EmployeeId WHERE [InvitationId] = @InvitationId AND [OrganizationId] = @OrganizationId;
+	UPDATE [Auth].[Invitation] SET [EmployeeId] = @EmployeeId WHERE [InvitationId] = @InvitationId AND [OrganizationId] = @OrganizationId
+	SELECT 2; -- Indicates employee id change was successful
+	END
