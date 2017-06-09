f8fa00ccfaf99f84b7e574c8613f690e227215f0
Ganeshkumar Miriyala <gkmiriyala@allyis.com>
2017-06-09 13:45:54 -0700

    reactivate

--- AllyisApps/Areas/TimeTracker/Controllers/Customer/ReactivateAction.cs
+++ AllyisApps/Areas/TimeTracker/Controllers/Customer/ReactivateAction.cs
@@ -39,4 +39,30 @@ namespace AllyisApps.Areas.TimeTracker.Controllers
             return this.RedirectToAction(ActionConstants.Index);
         }
     }
+
+    /// <summary>
+    /// POST: Customer/Delete.
+    /// </summary>
+    /// <param name="id">The Customer id.</param>
+    /// <returns>The Customer index.</returns>
+    public ActionResult Reactivate()
+    {
+        Customer customer = AppService.GetCustomer(id);
+
+        if (customer != null)
+        {
+            if (AppService.DeleteCustomer(id))
+            {
+                Notifications.Add(new BootstrapAlert(string.Format("{0} {1}", customer.Name, Resources.Strings.CustomerDeleteNotification), Variety.Success));
+
+                return this.RedirectToAction(ActionConstants.Index);
+            }
+
+            // Permission failure
+            Notifications.Add(new BootstrapAlert(Resources.Strings.ActionUnauthorizedMessage, Variety.Warning));
+        }
+
+        return this.RedirectToAction(ActionConstants.Index);
+    }
+}
 }
