e5ee77c8203f1fcd00685ef110c10448713d3c46
Ganeshkumar Miriyala <gkmiriyala@allyis.com>
2017-06-09 09:20:11 -0700

    reactivate action

---
+++ AllyisApps/Areas/TimeTracker/Controllers/Customer/ReactivateAction.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace AllyisApps.Areas.TimeTracker.Controllers.Customer
+{
+    public class ReactivateAction
+    {
+    }
+}
