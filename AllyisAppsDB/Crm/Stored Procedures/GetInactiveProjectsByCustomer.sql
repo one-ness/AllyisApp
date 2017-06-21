1e28f240552eb2a257da2b95ab1c5d1544ee76bc
Linh Dao <linh.dao@allyis.com>
2017-06-21 15:37:32 -0700

    no message

---
+++ AllyisAppsDB/Crm/Stored Procedures/GetInactiveProjectsByCustomer.sql
@@ -0,0 +1,14 @@
+﻿CREATE PROCEDURE [Crm].[GetInactiveProjectsByCustomer]
+	@CustomerId INT
+AS
+	SET NOCOUNT ON;
+	SELECT [Name],
+		   [ProjectId],
+		   [ProjectOrgId],
+		   [Type],
+		   [CustomerId],
+		   [StartUTC] AS [StartingDate],
+		   [EndUTC] AS [EndingDate]
+	FROM [Crm].[Project] WITH (NOLOCK) 
+	WHERE [IsActive] = 0 AND [CustomerId] = @CustomerId
+	ORDER BY [Project].[Name]
