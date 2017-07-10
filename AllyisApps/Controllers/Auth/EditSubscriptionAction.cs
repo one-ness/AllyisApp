9e6b157333f9341b622478112481f8916f13a00d
Ganeshkumar Miriyala <gkmiriyala@allyis.com>
2017-07-10 16:48:14 -0700

    EditSubscription files

---
+++ AllyisApps/Controllers/Auth/EditSubscriptionAction.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace AllyisApps.Controllers.Auth
+{
+	/// <summary>
+	/// Controller for account and organization related actions.
+	/// </summary>
+	public class EditSubscriptionAction : BaseController
+    {
+		/// <summary>
+		/// GET: /Account/EditSubscription.
+		/// </summary>
+		public ActionResult EditSubscription(int subscriptionId)
+        {
+            return View();
+        }
+    }
+}
---
+++ AllyisApps/ViewModels/Auth/EditSubscriptionViewModel.cs
@@ -0,0 +1,22 @@
+﻿//------------------------------------------------------------------------------
+// <copyright file="EditSubscriptionViewModel.cs" company="Allyis, Inc.">
+//     Copyright (c) Allyis, Inc.  All rights reserved.
+// </copyright>
+//------------------------------------------------------------------------------
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace AllyisApps.ViewModels.Auth
+{
+	/// <summary>
+	/// Represents an editable view of subcription.
+	/// </summary>
+	public class EditSubscriptionViewModel : BaseViewModel
+	{
+
+
+	}
+}
