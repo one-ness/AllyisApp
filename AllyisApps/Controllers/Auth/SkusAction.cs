bf55776a53ebba879f4e5162d6c1a574a021ccb4
Linh Dao <linh.dao@allyis.com>
2017-07-11 13:02:37 -0700

    Linked the Sub panel in MangeOrg to Skus page

---
+++ AllyisApps/Controllers/Auth/SkusAction.cs
@@ -0,0 +1,39 @@
+﻿//------------------------------------------------------------------------------
+// <copyright file="SkusAction.cs" company="Allyis, Inc.">
+//     Copyright (c) Allyis, Inc.  All rights reserved.
+// </copyright>
+//------------------------------------------------------------------------------
+
+using AllyisApps.Core.Alert;
+using AllyisApps.Services;
+using AllyisApps.Services.Billing;
+using AllyisApps.Services.Common.Types;
+using AllyisApps.ViewModels.Auth;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web.Mvc;
+
+namespace AllyisApps.Controllers
+{
+	/// <summary>
+	/// Controller for account and organization related actions.
+	/// </summary>
+	public partial class AccountController : BaseController
+	{
+		/// <summary>
+		/// GET: /account/skus/id
+		/// </summary>
+		/// <param name="id">The organization id</param>
+		public ActionResult Skus(int id)
+		{
+
+			this.AppService.CheckOrgAction(AppService.OrgAction.SubscribeToProduct, id);    //only org owner has permission
+			//var infos = AppService.GetProductSubscriptionInfo(id, productId);
+
+			SkusListViewModel model = new SkusListViewModel();
+
+			return this.View("Skus", model);
+		}		
+	}
+}
---
+++ AllyisApps/ViewModels/Auth/SkusListViewModel.cs
@@ -0,0 +1,15 @@
+﻿//------------------------------------------------------------------------------
+// <copyright file="SkusListViewModel.cs" company="Allyis, Inc.">
+//     Copyright (c) Allyis, Inc.  All rights reserved.
+// </copyright>
+//------------------------------------------------------------------------------
+
+namespace AllyisApps.ViewModels.Auth
+{
+	/// <summary>
+	/// Represents the Skus List view
+	/// </summary>
+	public class SkusListViewModel : BaseViewModel
+	{
+	}
+}
