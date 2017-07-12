89ddfd2bd282e7f71a8bc1bc48c1b36a29117c3f
Ganeshkumar Miriyala <gkmiriyala@allyis.com>
2017-07-12 12:49:11 -0700

    EditSubscription

--- AllyisApps/Controllers/Auth/EditSubscriptionAction.cs
+++ AllyisApps/Controllers/Auth/EditSubscriptionAction.cs
@@ -1,6 +1,7 @@
 ﻿
 using AllyisApps.Core.Alert;
 using AllyisApps.Services;
+using AllyisApps.Services.Billing;
 using AllyisApps.ViewModels.Auth;
 using System;
 using System.Collections.Generic;
@@ -18,10 +19,19 @@ namespace AllyisApps.Controllers
 		/// </summary>
 		/// <returns>The result of this action.</returns>
 		[HttpGet]
-		public ActionResult EditSubscription(int id, int idTwo)
+		public ActionResult EditSubscription(int id, int skuId)
         {
-			this.AppService.CheckOrgAction(AppService.OrgAction.EditSubscription, id);
-			return this.View(ViewConstants.EditSubscription, idTwo);
+			int orgId = AppService.UserContext.UserSubscriptions[id].OrganizationId;
+			this.AppService.CheckOrgAction(AppService.OrgAction.EditSubscription, orgId);
+			SkuInfo sku = AppService.GetSkuDetails(skuId);
+			EditSubscriptionViewModel model = new EditSubscriptionViewModel
+			{
+				SkuId = sku.SkuId,
+				Name = sku.Name,
+				Description = sku.Description,
+				SubscriptionId = sku.SubscriptionId
+			};
+			return this.View(ViewConstants.EditSubscription, model);
 		}
     }
 }
