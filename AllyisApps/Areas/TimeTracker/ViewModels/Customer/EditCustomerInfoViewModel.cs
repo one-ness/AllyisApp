d097ee1058de5f3105783bfd89890f4ee18cf077
Ganeshkumar Miriyala <gkmiriyala@allyis.com>
2017-07-21 12:58:09 -0700

    update regex for phone and fax for creating customer

--- AllyisApps/Areas/TimeTracker/ViewModels/Customer/EditCustomerInfoViewModel.cs
+++ AllyisApps/Areas/TimeTracker/ViewModels/Customer/EditCustomerInfoViewModel.cs
@@ -54,9 +54,15 @@ namespace AllyisApps.ViewModels.TimeTracker.Customer
 		/// <summary>
 		/// Gets or sets the user's phone number.
 		/// </summary>
-		[RegularExpression(@"^\(?([0-9]{3})\)?[-. ]?([0-9]{3})[-. ]?([0-9]{4})$", ErrorMessage = "Entered phone format is not valid.")] // [Phone] does not work
+		[RegularExpression(@"^(?:(?:\+?1\s*(?:[.-]\s*)?)?(?:\(\s*([2-9]1[02-9]|[2-9][02-8]1|[2-9][02-8][02-9])\s*\)|([2-9]1[02-9]|[2-9][02-8]1|[2-9][02-8][02-9]))\s*(?:[.-]\s*)?)?([2-9]1[02-9]|[2-9][02-9]1|[2-9][02-9]{2})\s*(?:[.-]\s*)?([0-9]{4})(?:\s*(?:#|x\.?|ext\.?|extension)\s*(\d+))?$", ErrorMessage = "Entered phone format is not valid.")] // [Phone] does not work
 		[Display(Name = "Contact Phone Number")]
 		public string ContactPhoneNumber { get; set; }
+
+		/// <summary>
+		/// Gets or sets the Customer's fax number.
+		/// </summary>
+		[RegularExpression(@"^(?:(?:\+?1\s*(?:[.-]\s*)?)?(?:\(\s*([2-9]1[02-9]|[2-9][02-8]1|[2-9][02-8][02-9])\s*\)|([2-9]1[02-9]|[2-9][02-8]1|[2-9][02-8][02-9]))\s*(?:[.-]\s*)?)?([2-9]1[02-9]|[2-9][02-9]1|[2-9][02-9]{2})\s*(?:[.-]\s*)?([0-9]{4})(?:\s*(?:#|x\.?|ext\.?|extension)\s*(\d+))?$", ErrorMessage = "Entered phone format is not valid.")] // [Phone] does not work
+		public string FaxNumber { get; set; }
 
 		/// <summary>
 		/// Gets or sets the Customer's phone extension.
@@ -124,11 +130,6 @@ namespace AllyisApps.ViewModels.TimeTracker.Customer
 		/// Gets or sets the name of the organization that the Customer belongs too.
 		/// </summary>
 		public string OrganizationName { get; set; }
-
-		/// <summary>
-		/// Gets or sets the Customer's fax number.
-		/// </summary>
-		public string FaxNumber { get; set; }
 
 		/// <summary>
 		/// Gets or sets the Customer's Web url.
