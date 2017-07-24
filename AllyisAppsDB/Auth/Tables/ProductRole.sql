5a9744ba688c2bef097cb05c70987c10ae21bf6c
shankarab <shankarab@gmail.com>
2017-07-24 14:26:51 -0700

    first draft of expense tracker db

--- AllyisAppsDB/Auth/Tables/ProductRole.sql
+++ AllyisAppsDB/Auth/Tables/ProductRole.sql
@@ -1,11 +1,13 @@
 ﻿CREATE TABLE [Auth].[ProductRole] (
     [ProductRoleId]   INT           NOT NULL,
-    [ProductId]       SMALLINT           NOT NULL,
-    [Name]            NVARCHAR (32) NOT NULL,
-    [PermissionAdmin] BIT           DEFAULT ((0)) NOT NULL,
-    PRIMARY KEY CLUSTERED ([ProductRoleId] ASC),
+    [ProductId]       SMALLINT      NOT NULL,
+    [Name]            NVARCHAR (64) NOT NULL,
+    [PermissionAdmin] BIT           CONSTRAINT [DF__ProductRo__Permi__5812160E] DEFAULT ((0)) NOT NULL,
+    CONSTRAINT [PK__ProductR__E5D91894EF4B4E0C] PRIMARY KEY CLUSTERED ([ProductRoleId] ASC),
     CONSTRAINT [FK_ProductRole_Product] FOREIGN KEY ([ProductId]) REFERENCES [Billing].[Product] ([ProductId])
 );
+
+
 
 GO
 CREATE NONCLUSTERED INDEX [IX_FK_ProductRole]
---
+++ AllyisAppsDB/Finance/Tables/AccountType.sql
@@ -0,0 +1,6 @@
+﻿CREATE TABLE [Finance].[AccountType] (
+    [AccountTypeId]   TINYINT       NOT NULL,
+    [AccountTypeName] NVARCHAR (32) NOT NULL,
+    CONSTRAINT [PK_AccountType] PRIMARY KEY CLUSTERED ([AccountTypeId] ASC)
+);
+
