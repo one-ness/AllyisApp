8dd5e58ed49c455b51d191d8e22ee053f7e6df68
shankarab <shankarab@gmail.com>
2017-07-25 14:11:18 -0700

    expense tracker skeleton code

---
+++ AllyisApps/Areas/ExpenseTracker/ViewModels/Expense/ExpenseReportsViewModel.cs
@@ -0,0 +1,11 @@
+﻿using AllyisApps.ViewModels;
+
+namespace AllyisApps.Areas.ExpenseTracker.ViewModels.Expense
+{
+	/// <summary>
+	/// list of all the expense reports submitted by the user
+	/// </summary>
+	public class ExpenseReportsViewModel : BaseViewModel
+	{
+	}
+}
