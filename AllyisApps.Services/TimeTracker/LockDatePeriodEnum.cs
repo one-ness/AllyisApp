3120d32a6a8e7c6f42d4e4364b914445f8325339
Ganeshkumar Miriyala <gkmiriyala@allyis.com>
2017-07-28 19:29:13 -0700

    Added Enum files for LockDatePeriod and PayClassId

---
+++ AllyisApps.Services/TimeTracker/LockDatePeriodEnum.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AllyisApps.Services.Auth
+{
+	public enum LockDatePeriodEnum : int
+	{
+		/// <summary>
+		/// Days
+		/// </summary>
+		days = 1,
+
+		/// <summary>
+		/// Weeks
+		/// </summary>
+		weeks = 2,
+
+		/// <summary>
+		/// Weeks
+		/// </summary>
+		months = 3
+	}
+}
---
+++ AllyisApps.Services/TimeTracker/PayClassIdEnum.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AllyisApps.Services.Auth
+{
+	public enum PayClassId : int
+	{
+		/// <summary>
+		/// Regular.
+		/// </summary>
+		Regular = 1,
+
+		/// <summary>
+		/// Paid Time Off.
+		/// </summary>
+		PaidTimeOff = 2,
+
+		/// <summary>
+		/// Unpaid Time Off.
+		/// </summary>
+		UnpaidTimeOff = 3,
+
+		/// <summary>
+		/// Holiday.
+		/// </summary>
+		Holiday = 4,
+
+		/// <summary>
+		/// Bereavement Leave.
+		/// </summary>
+		BereavementLeave = 5,
+
+		/// <summary>
+		/// Jury Duty.
+		/// </summary>
+		JuryDuty = 6,
+
+		/// <summary>
+		/// Overtime.
+		/// </summary>
+		Overtime = 7,
+
+		/// <summary>
+		/// Other Leave.
+		/// </summary>
+		OtherLeave = 8,
+	}
+}
