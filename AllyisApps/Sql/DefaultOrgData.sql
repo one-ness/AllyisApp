cca601b7ba41c9752dd2ae201d2aef44f8145c03
shankarab <shankarab@gmail.com>
2017-07-31 17:49:50 -0700

    creating file for default data that should be added for every newly created org

---
+++ AllyisApps/Sql/DefaultOrgData.sql
@@ -0,0 +1,8 @@
+﻿/*
+	Contains data that is provided to every newly created organization as default data. the user is free to change this afterward through the UI
+*/
+-- add employee types
+
+-- add pay classes
+
+-- add roles and permissions
