f5490b4a97bf58be59ab6bd9adcc0f2850387757
shankarab <shankarab@gmail.com>
2017-07-31 17:43:17 -0700

    increasing the country name column length

--- AllyisAppsDB/Lookup/Tables/State.sql
+++ AllyisAppsDB/Lookup/Tables/State.sql
@@ -1,7 +1,7 @@
 ﻿CREATE TABLE [Lookup].[State] (
     [StateId]   INT           IDENTITY (1, 1) NOT NULL,
     [Code]      VARCHAR (8)   NOT NULL,
-    [Name]      NVARCHAR (64) NOT NULL,
+    [Name]      NVARCHAR (96) NOT NULL,
     [CountryId] INT           NULL,
     CONSTRAINT [PK_State] PRIMARY KEY NONCLUSTERED ([StateId] ASC),
     CONSTRAINT [FK_State_Country] FOREIGN KEY ([CountryId]) REFERENCES [Lookup].[Country] ([CountryId]),
