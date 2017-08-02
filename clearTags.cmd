8ef9851a361f1e4e40a152cf037efd2d34394bad
Ryan Leenay <ryan.leenay@allyis.com>
2017-08-02 09:53:28 -0700

    Slight syntax change when useing as file.

--- clearTags.cmd
+++ clearTags.cmd
@@ -1,10 +1,5 @@
-#Use this to delete tags from within Source Trees Terminal Window currenlty 
-#Delete local tags.
-FOR /f %a in ('git tag -l') DO git tag -d %a 
-#Fetch remote tags.
+FOR /f %%a in ('git tag -l') DO git tag -d %%a 
 git fetch
-#Delete remote tags.
-FOR /f %a in ('git tag -l') DO git push --delete origin %a 
-#Delete local tasg.
-FOR /f %a in ('git tag -l') DO git tag -d %a 
-
+FOR /f %%a in ('git tag -l') DO git push --delete origin %%a 
+FOR /f %%a in ('git tag -l') DO git tag -d %%a 
+pause
