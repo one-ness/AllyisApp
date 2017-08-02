4995331119543165a606795b18e34b7e7bd93861
John Nortz <john.nortz@allyis.com>
2017-08-01 17:54:25 -0700

    Removed DefaultData/PayClasses.sql,  Added replacement script which adds the defaults, linked to the org, whenever an org subscribes to TimeTracker

---
+++ AllyisAppsDB/TimeTracker/Stored Procedures/CreateDefaultPayClass.sql
@@ -0,0 +1,30 @@
+﻿CREATE PROCEDURE [Hrm].[CreateDefaultPayClass]
+	@organizationId INT
+AS
+	SET NOCOUNT ON;
+
+	INSERT INTO [Hrm].[PayClass] ([Name], [OrganizationId])
+	VALUES ('Regular', @OrganizationId);
+
+	INSERT INTO [Hrm].[PayClass] ([Name], [OrganizationId])
+	VALUES ('Paid Time Off', @OrganizationId);
+
+	INSERT INTO [Hrm].[PayClass] ([Name], [OrganizationId])
+	VALUES ('Unpaid Time Off', @OrganizationId);
+
+	INSERT INTO [Hrm].[PayClass] ([Name], [OrganizationId])
+	VALUES ('Holiday', @OrganizationId);
+
+	INSERT INTO [Hrm].[PayClass] ([Name], [OrganizationId])
+	VALUES ('Bereavement Leave', @OrganizationId);
+
+	INSERT INTO [Hrm].[PayClass] ([Name], [OrganizationId])
+	VALUES ('Jury Duty', @OrganizationId);
+
+	INSERT INTO [Hrm].[PayClass] ([Name], [OrganizationId])
+	VALUES ('Overtime', @OrganizationId);
+
+	INSERT INTO [Hrm].[PayClass] ([Name], [OrganizationId])
+	VALUES ('Other Leave', @OrganizationId);
+
+	SELECT SCOPE_IDENTITY();
