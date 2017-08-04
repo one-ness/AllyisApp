b33ee8cc3a5e81d7c65f28a40fa947bc0d5e6474
Ryan Leenay <ryan.leenay@allyis.com>
2017-08-04 15:28:18 -0700

    Replace wiht calcluated column and function.

---
+++ AllyisAppsDB/Billing/Stored Procedures/GetNumberSubscriptionUsers.sql
@@ -0,0 +1,6 @@
+﻿CREATE FUNCTION [Billing].[GetNumberSubscriptionUsers](@subscriptoinID Int)
+RETURNS INT
+AS
+BEGIN
+	Return (SELECT COUNT(*) FROM [Billing].[SubscriptionUser] [s] Where [s].SubscriptionId = @subscriptoinID); 
+END
