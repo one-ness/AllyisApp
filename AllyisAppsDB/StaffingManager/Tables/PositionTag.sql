a55c85c409ed2e29f67fd4e02e9dc262e33d4941
John Nortz <john.nortz@allyis.com>
2017-08-09 15:25:54 -0700

    build this time

--- AllyisAppsDB/StaffingManager/Tables/PositionTag.sql
+++ AllyisAppsDB/StaffingManager/Tables/PositionTag.sql
@@ -1,7 +1,7 @@
 CREATE TABLE [StaffingManager].[PositionTag] (
-	[PositionId]		INT		         CONSTRAINT [DF__PositionTag_PositionId] DEFAULT ((0)) NOT NULL,
-	[TagId]				INT				 CONSTRAINT [DF__PositionTag_TagId] DEFAULT ((0)) NOT NULL,
-	CONSTRAINT [FK_PositionTag_PositionId] FOREIGN KEY ([PositionId]) REFERENCES [StaffingManager].[Position] ([PositionId]),
-	CONSTRAINT [FK_PositionTag_TagId] FOREIGN KEY ([TagId]) REFERENCES [StaffingManager].[Tag] ([TagId])
+	[PositionId]		INT		         CONSTRAINT [DF_PositionTag_Position] DEFAULT ((0)) NOT NULL,
+	[TagId]				INT				 CONSTRAINT [DF_PositionTag_Tag] DEFAULT ((0)) NOT NULL,
+	CONSTRAINT [FK_PositionTag_Position] FOREIGN KEY ([PositionId]) REFERENCES [StaffingManager].[Position] ([PositionId]),
+	CONSTRAINT [FK_PositionTag_Tag] FOREIGN KEY ([TagId]) REFERENCES [StaffingManager].[Tag] ([TagId])
 );
 
