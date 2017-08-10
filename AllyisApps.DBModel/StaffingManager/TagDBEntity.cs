7b6df5aed34180b426cc33bb7bab6d2825763c12
John Nortz <john.nortz@allyis.com>
2017-08-10 11:26:52 -0700

    DB entities for tags and positions, an stored procedures

---
+++ AllyisApps.DBModel/StaffingManager/TagDBEntity.cs
@@ -0,0 +1,24 @@
+﻿namespace AllyisApps.DBModel.StaffingManager
+{
+	/// <summary>
+	/// Represents the Address table in the database.
+	/// </summary>
+	public class TagDBEntity
+    {
+        /// <summary>
+        /// Gets or sets the Tags Id
+        /// </summary>
+        public int TagId { get; set; }
+
+        /// <summary>
+        /// Gets or sets Position Id
+        /// </summary>
+        public int PositionId { get; set; }
+
+        /// <summary>
+        /// Gets or sets the Tag's Name
+        /// </summary>
+        public string TagName { get; set; }
+        
+    }
+}
