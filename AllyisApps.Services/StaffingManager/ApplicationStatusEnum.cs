99ef8fa32d4db90736919b4a4efd6ea7ae6dc6de
Luke Selbeck <luke.selbeck@allyis.com>
2017-08-15 15:44:45 -0700

    Add service layer objects to Staffing Manager: Applicant, Application, and ApplicationDocument

---
+++ AllyisApps.Services/StaffingManager/ApplicationStatusEnum.cs
@@ -0,0 +1,20 @@
+﻿namespace AllyisApps.Services.StaffingManager
+{
+	public enum ApplicationStatusEnum : int
+	{
+		/// <summary>
+		/// In Progress.
+		/// </summary>
+		InProgress,
+
+		/// <summary>
+		/// Accepted.
+		/// </summary>
+		Accepted,
+
+		/// <summary>
+		/// Rejected.
+		/// </summary>
+		Rejected,
+	}
+}
