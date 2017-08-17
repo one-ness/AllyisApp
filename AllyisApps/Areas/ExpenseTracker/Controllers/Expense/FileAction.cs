c8143785d46041b9be9ecafc7dc3b468372aa2f6
Avery Pratt <avery.pratt@allyis.com>
2017-08-17 11:38:58 -0700

    temp commit - computer restart required

---
+++ AllyisApps/Areas/ExpenseTracker/Controllers/Expense/FileAction.cs
@@ -0,0 +1,29 @@
+﻿using AllyisApps.Controllers;
+using AllyisApps.Services;
+using System.Web.Mvc;
+using System.Threading.Tasks;
+using AllyisApps.ViewModels.ExpenseTracker.Expense;
+using System.Collections.Generic;
+using System;
+using System.Linq;
+using AllyisApps.DBModel;
+using AllyisApps.DBModel.Finance;
+
+namespace AllyisApps.Areas.ExpenseTracker.Controllers
+{
+	/// <summary>
+	/// Stores a new report in database and redirects to View
+	/// </summary>
+	public partial class ExpenseController : BaseController
+	{
+		/// <summary>
+		/// Displays a file associated with report.
+		/// </summary>
+		/// <param name="model"></param>
+		/// <returns></returns>
+		public ActionResult DisplayFile(ExpenseCreateModel model)
+		{
+			return PartialView("_AjaxExpenseReportFile", model);
+		}
+	}
+}
