cebfc2e61bf719e2fe3fdec7bdc50dea08b884e4
Avery Pratt <avery.pratt@allyis.com>
2017-08-28 13:11:00 -0700

    added scaffolding for pending page

---
+++ AllyisApps/Areas/ExpenseTracker/Controllers/Expense/PendingAction.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Web.Mvc;
+using AllyisApps.Controllers;
+using AllyisApps.Services;
+using AllyisApps.ViewModels.ExpenseTracker.Expense;
+using System.Web;
+using AllyisApps.Services.Expense;
+using System.IO;
+using AllyisApps.Lib;
+
+namespace AllyisApps.Areas.ExpenseTracker.Controllers
+{
+	/// <summary>
+	/// Creates a new report.
+	/// </summary>
+	public partial class ExpenseController : BaseController
+	{
+		/// <summary>
+		/// Create expense report.
+		/// </summary>
+		/// <param name="subscriptionId">The subscription id.</param>
+		/// <returns>Returns an action result.</returns>
+		public ActionResult Pending(int subscriptionId)
+		{
+			var model = new ExpensePendingModel()
+			{
+
+			};
+			return View(model);
+		}
+	}
+}
