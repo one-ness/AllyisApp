b32e93f10e5bc808bb0d77624677e18f7f3c05ef
Avery Pratt <avery.pratt@allyis.com>
2017-08-28 10:17:34 -0700

    worked out post-merge issues

--- AllyisApps.Services/Auth/AccountService.cs
+++ AllyisApps.Services/Auth/AccountService.cs
@@ -4,13 +4,6 @@
 // </copyright>
 //------------------------------------------------------------------------------
 
-using AllyisApps.DBModel;
-using AllyisApps.DBModel.Auth;
-using AllyisApps.DBModel.Billing;
-using AllyisApps.DBModel.Lookup;
-using AllyisApps.Lib;
-using AllyisApps.Services.Auth;
-using AllyisApps.Services.Lookup;
 using System;
 using System.Collections;
 using System.Collections.Generic;
@@ -21,8 +14,10 @@ using System.Threading.Tasks;
 using AllyisApps.DBModel;
 using AllyisApps.DBModel.Auth;
 using AllyisApps.DBModel.Billing;
+using AllyisApps.DBModel.Finance;
 using AllyisApps.DBModel.Lookup;
 using AllyisApps.Lib;
+using AllyisApps.Services.Auth;
 using AllyisApps.Services.Lookup;
 
 namespace AllyisApps.Services
