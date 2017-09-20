962cea129be1a48bb3016069b35859719fe00605
shankarab <shankarab@gmail.com>
2017-09-20 16:09:15 -0700

    organizationuser class added

---
+++ AllyisApps.Services/Auth/OrganizationUser.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AllyisApps.Services
+{
+	/// <summary>
+	/// user in an organization
+	/// </summary>
+	public class OrganizationUser : User
+	{
+	}
+}
