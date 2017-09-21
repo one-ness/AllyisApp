44a6c756ef5210551d2e63811e2ece0eca473d1e
Ryan Leenay <ryan.leenay@allyis.com>
2017-09-21 13:52:38 -0700

    FACEPALM small stored procedure change added to Production.

--- AllyisAppsDB/Auth/Stored Procedures/GetUserInvitationsByInviteId.sql
+++ AllyisAppsDB/Auth/Stored Procedures/GetUserInvitationsByInviteId.sql
@@ -7,13 +7,13 @@ AS
 		[Email],
 		[FirstName],
 		[LastName], 
-		[OrganizationId], 
+		[Invitation].[OrganizationId], 
 		[Invitation].[OrganizationRoleId],
-		[Auth].[Organization].[OrganizationName],
+		[Organization].[OrganizationName],
 		[Auth].[OrganizationRole].[OrganizationRoleName],
 		[EmployeeId],
 		[Invitation].[StatusId]
 	FROM [Auth].[Invitation] WITH (NOLOCK)
 	LEFT JOIN [Auth].[OrganizationRole] WITH (NOLOCK) ON [OrganizationRole].[OrganizationRoleId] = [Invitation].[OrganizationRoleId]
-	LEFT JOIN [Auth].[Organizaion] WITH (NOLOCK) ON [Auth].[Organization].OrganizationId =  [Invitation].[OrganizationId] 
-	WHERE [InvitationId] = @inviteId AND [IsActive] = 1
+	LEFT JOIN [Organization] WITH (NOLOCK) ON [Organization].[OrganizationId] =  [Invitation].[OrganizationId] 
+	WHERE [InvitationId] = @inviteId AND [Invitation].IsActive = 1
