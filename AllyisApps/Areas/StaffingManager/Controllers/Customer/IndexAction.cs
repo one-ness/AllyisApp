e8511cf824777c215dcc4a911264c41aad8ba136
Ryan Leenay <ryan.leenay@allyis.com>
2017-09-25 15:03:27 -0700

    Ops for got area

--- AllyisApps/Areas/StaffingManager/Controllers/Customer/IndexAction.cs
+++ AllyisApps/Areas/StaffingManager/Controllers/Customer/IndexAction.cs
@@ -10,6 +10,7 @@ using System.Web.Mvc;
 using AllyisApps.Controllers;
 using AllyisApps.Services;
 using AllyisApps.Services.Auth;
+using AllyisApps.Services.Billing;
 using AllyisApps.Services.Crm;
 using AllyisApps.ViewModels.TimeTracker.Customer;
 
