fbb0be5fa932ea6c6d9ef0066905cfd6d91641fc
Ryan Leenay <ryan.leenay@allyis.com>
2017-09-26 17:05:47 -0700

    Services removed from skus view model

--- AllyisApps/ViewModels/BillingProductSubscription/SubscriptionDisplayViewModel.cs
+++ AllyisApps/ViewModels/BillingProductSubscription/SubscriptionDisplayViewModel.cs
@@ -4,7 +4,6 @@
 // </copyright>
 //------------------------------------------------------------------------------
 
-using AllyisApps.Services;
 using AllyisApps.Services.Billing;
 
 namespace AllyisApps.ViewModels.Auth
