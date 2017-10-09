8c261586b63bba16a92ee348f16b6943a284883d
Avery Pratt <avery.pratt@allyis.com>
2017-10-09 11:16:44 -0700

    added remaining files

--- AllyisAppsDB/UpgradeScripts/AddSkuIconUrl.sql
+++ AllyisAppsDB/UpgradeScripts/AddSkuIconUrl.sql
@@ -1,12 +1,12 @@
-﻿UPDATE [Billing].[Sku] SET [IconUrl] = 'Content/TimeTracker/icons/TimeTracker.png' WHERE [SkuId] = 200001;
+﻿UPDATE [Billing].[Sku] SET [IconUrl] = 'Content/icons/TimeTracker.png' WHERE [SkuId] = 200001;
 
 Go
 -- Expense Tracker Skus --
-Update  [Billing].[Sku] SET [IconUrl] =  'Content/ExpenseTracker/icons/ExpenseTracker.png' WHERE [SkuID] = 300001;
+Update  [Billing].[Sku] SET [IconUrl] =  'Content/icons/ExpenseTracker.png' WHERE [SkuID] = 300001;
 
 Go
 
 -- Staffing Skus --
-UPDATE [Billing].[Sku] SET [IconUrl] = 'Content/StaffingManager/icons/StaffingManager.png' WHERE [SkuID] = 400001;
+UPDATE [Billing].[Sku] SET [IconUrl] = 'Content/icons/StaffingManager.png' WHERE [SkuID] = 400001;
 
 Go
