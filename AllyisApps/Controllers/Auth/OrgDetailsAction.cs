96a67a3401275cae57cc69a6762cc084e79263af
John Nortz <john.nortz@allyis.com>
2017-10-11 13:01:20 -0700

    merge conflicts?

---
+++ AllyisApps/Controllers/Auth/OrgDetailsAction.cs
@@ -0,0 +1,48 @@
+﻿//------------------------------------------------------------------------------
+// <copyright file="OrgDetailsAction.cs" company="Allyis, Inc.">
+//     Copyright (c) Allyis, Inc.  All rights reserved.
+// </copyright>
+//------------------------------------------------------------------------------
+
+using System;
+using System.Linq;
+using System.Web.Mvc;
+using AllyisApps.Lib;
+using AllyisApps.Services;
+using AllyisApps.Services.Auth;
+using AllyisApps.Services.Billing;
+using AllyisApps.Services.Common.Types;
+using AllyisApps.ViewModels.Auth;
+
+namespace AllyisApps.Controllers.Auth
+{
+	/// <summary>
+	/// Controller for account and organization related actions.
+	/// </summary>
+	public partial class AccountController : BaseController
+	{
+		/// <summary>
+		/// Get: Account/OrgDetails
+		/// </summary>
+		public ActionResult OrgDetails(int id)
+		{
+			var model = new OrganizationDetailsViewModel();
+			var org = this.AppService.GetOrganization(id);
+			if (org.Address != null)
+			{
+				model.Address = org.Address.Address1;
+				model.City = org.Address.City;
+				model.CountryName = org.Address.CountryName;
+				model.PostalCode = org.Address.PostalCode;
+				model.StateName = org.Address.StateName;
+			}
+
+			model.FaxNumber = org.FaxNumber;
+			model.OrganizationId = org.OrganizationId;
+			model.OrganizationName = org.OrganizationName;
+			model.PhoneNumber = org.PhoneNumber;
+			model.SiteURL = org.SiteUrl;
+			return View(model);
+		}
+	}
+}
---
+++ AllyisApps/Controllers/Auth/OrgSubscriptionsAction.cs
@@ -0,0 +1,50 @@
+﻿//------------------------------------------------------------------------------
+// <copyright file="OrgSubscriptionsAction.cs" company="Allyis, Inc.">
+//     Copyright (c) Allyis, Inc.  All rights reserved.
+// </copyright>
+//------------------------------------------------------------------------------
+
+using System;
+using System.Linq;
+using System.Web.Mvc;
+using AllyisApps.Lib;
+using AllyisApps.Services;
+using AllyisApps.Services.Auth;
+using AllyisApps.Services.Billing;
+using AllyisApps.Services.Common.Types;
+using AllyisApps.ViewModels.Auth;
+using System.Threading.Tasks;
+
+namespace AllyisApps.Controllers.Auth
+{
+	/// <summary>
+	/// Controller for account and organization related actions.
+	/// </summary>
+	public partial class AccountController : BaseController
+	{
+		/// <summary>
+		/// Get: Account/OrgSubscriptions
+		/// </summary>
+		public async Task<ActionResult> OrgSubscriptions(int id)
+		{
+			var model = new OrganizationSubscriptionsViewModel();
+			model.CanEditSubscriptions = this.AppService.CheckOrgAction(AppService.OrgAction.EditSubscription, id, false);
+			model.OrganizationId = id;
+			var collection = await this.AppService.GetSubscriptions(id);
+			foreach (var item in collection)
+			{
+				var data = new OrganizationSubscriptionsViewModel.ViewModelItem();
+				data.AreaUrl = item.AreaUrl;
+				data.NumberofUsers = item.NumberOfUsers;
+				data.ProductDescription = item.ProductDescription;
+				data.ProductName = item.ProductName;
+				data.SubscriptionCreatedUtc = item.SubscriptionCreatedUtc;
+				data.SubscriptionId = item.SubscriptionId;
+				data.SubscriptionName = item.SubscriptionName;
+				model.Subscriptions.Add(data);
+			}
+
+			return View(model);
+		}
+	}
+}
