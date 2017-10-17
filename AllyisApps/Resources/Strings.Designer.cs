e53ecf220765bb035551dfce5a2fe3e26cfee4f7
Luke Selbeck <luke.selbeck@allyis.com>
2017-10-17 15:01:19 -0700

    Style changes, and filter added

--- AllyisApps/Resources/Strings.Designer.cs
+++ AllyisApps/Resources/Strings.Designer.cs
@@ -3598,6 +3598,15 @@ namespace AllyisApps.Resources {
         }
         
         /// <summary>
+        ///   Looks up a localized string similar to Status.
+        /// </summary>
+        public static string Status {
+            get {
+                return ResourceManager.GetString("Status", resourceCulture);
+            }
+        }
+        
+        /// <summary>
         ///   Looks up a localized string similar to An error has occurred..
         /// </summary>
         public static string StatusErrorMessage {
