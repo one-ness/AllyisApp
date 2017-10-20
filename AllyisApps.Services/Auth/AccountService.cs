8e26d7769e0995a7d49e021b2bb9b5a77aa63f4e
shankarab <shankarab@gmail.com>
2017-10-19 17:01:06 -0700

    account service may not build

--- AllyisApps.Services/Auth/AccountService.cs
+++ AllyisApps.Services/Auth/AccountService.cs
@@ -332,6 +332,12 @@ namespace AllyisApps.Services
 					}
 				).ToList();
 
+			if (this.UserContext.UserId != userInfo.UserId)
+			{
+				// logged in user is trying to read a different user's information
+
+			}
+
 			return userInfo;
 		}
 
@@ -340,7 +346,7 @@ namespace AllyisApps.Services
 		/// </summary>
 		public void UpdateCurrentUserProfile(int? dateOfBirth, string firstName, string lastName, string phoneNumber, int? addressId, string address, string city, int? stateId, string postalCode, string countryCode)
 		{
-			this.DBHelper.UpdateUserProfile(this.UserContext.UserId, firstName, lastName, Utility.GetNullableDateTimeFromDays(dateOfBirth), phoneNumber, addressId, address, null, city, stateId, postalCode, countryCode);
+			this.DBHelper.UpdateUserProfile(this.UserContext.UserId, firstName, lastName, Utility.GetDateTimeFromDays(dateOfBirth), phoneNumber, addressId, address, null, city, stateId, postalCode, countryCode);
 		}
 
 		/// <summary>
@@ -348,7 +354,7 @@ namespace AllyisApps.Services
 		/// </summary>
 		public void UpdateUserProfile(int userId, int? dateOfBirth, string firstName, string lastName, string phoneNumber, int? addressId, string address, string city, int? stateId, string postalCode, string countryCode)
 		{
-			this.DBHelper.UpdateUserProfile(userId, firstName, lastName, Utility.GetNullableDateTimeFromDays(dateOfBirth), phoneNumber, addressId, address, null, city, stateId, postalCode, countryCode);
+			this.DBHelper.UpdateUserProfile(userId, firstName, lastName, Utility.GetDateTimeFromDays(dateOfBirth), phoneNumber, addressId, address, null, city, stateId, postalCode, countryCode);
 		}
 
 		/// <summary>
