02f7a1c30126e7809006c8b9d73f5c9fc85f3fb3
Avery Pratt <avery.pratt@allyis.com>
2017-11-02 16:30:02 -0700

    sorted states alphabetically and fixed dictionary lookup bug for countries that have no states

--- AllyisApps.Services/Auth/AccountService.cs
+++ AllyisApps.Services/Auth/AccountService.cs
@@ -42,7 +42,14 @@ namespace AllyisApps.Services
 		/// </summary>
 		public List<State> GetStates(string countryCode)
 		{
-			return CacheContainer.StatesCache[countryCode];
+			try
+			{
+				return CacheContainer.StatesCache[countryCode].OrderBy(s => s.StateName).ToList();
+			}
+			catch (KeyNotFoundException)
+			{
+				return new List<State>();
+			}
 		}
 
 		public bool DeleteExpenseItem(int itemId)
