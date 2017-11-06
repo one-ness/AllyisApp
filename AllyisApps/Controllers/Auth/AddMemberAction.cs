b1992c2b31fad50a1b2c054fec4181dd87b65f61
Brian Fleck <bfleck@allyis.com>
2017-11-06 15:20:03 -0800

    Changes to Invitations to display organization name. Added dialog boxes for confirmation when deleting members and invitations.

--- AllyisApps/Controllers/Auth/AddMemberAction.cs
+++ AllyisApps/Controllers/Auth/AddMemberAction.cs
@@ -18,135 +18,139 @@ using AllyisApps.ViewModels.Auth;
 
 namespace AllyisApps.Controllers.Auth
 {
-	/// <summary>
-	/// Controller for account and organization related actions.
-	/// </summary>
-	public partial class AccountController : BaseController
-	{
-		/// <summary>
-		/// GET: /Add.
-		/// The page for adding members to an organization.
-		/// </summary>
-		public async Task<ActionResult> AddMember(int organizationId)
-		{
-			AppService.CheckOrgAction(AppService.OrgAction.AddUserToOrganization, organizationId);
-			var subs = await AppService.GetSubscriptionsAsync(organizationId);
+    /// <summary>
+    /// Controller for account and organization related actions.
+    /// </summary>
+    public partial class AccountController : BaseController
+    {
+        /// <summary>
+        /// GET: /Add.
+        /// The page for adding members to an organization.
+        /// </summary>
+        public async Task<ActionResult> AddMember(int organizationId)
+        {
+            AppService.CheckOrgAction(AppService.OrgAction.AddUserToOrganization, organizationId);
+            var subs = await AppService.GetSubscriptionsAsync(organizationId);
 
-			var model = new AddMemberViewModel
-			{
-				OrganizationId = organizationId,
-				EmployeeId = await AppService.GetNextEmployeeId(organizationId),
-				OrganizationName = "Organization", //AppService.GetOrganization(id).Result.OrganizationName;
-				SubscriptionRoles = subs.Select(sub => new RoleItem
-				{
-					ProductId = (int)sub.ProductId,
-					SubscriptionName = sub.SubscriptionName,
-					SelectList = GetSubRoles(sub.SkuId)
-				}).ToList(),
-				OrgRole = new SelectList(
-					new List<SelectListItem>
-					{
-						new SelectListItem { Text = OrganizationRoleEnum.Member.ToString(), Value = "1"},
-						new SelectListItem { Text = OrganizationRoleEnum.Owner.ToString(), Value = "2" }
-					},
-					"Value",
-					"Text",
-					"1")
-			};
+            var model = new AddMemberViewModel
+            {
+                OrganizationId = organizationId,
+                EmployeeId = await AppService.GetNextEmployeeId(organizationId),
+                OrganizationName = "Organization", //AppService.GetOrganization(id).Result.OrganizationName;
+                SubscriptionRoles = subs.Select(sub => new RoleItem
+                {
+                    ProductId = (int)sub.ProductId,
+                    SubscriptionName = sub.SubscriptionName,
+                    SelectList = GetSubRoles(sub.SkuId)
+                }).ToList(),
+                OrgRole = new SelectList(
+                    new List<SelectListItem>
+                    {
+                        new SelectListItem { Text = OrganizationRoleEnum.Member.ToString(), Value = "1"},
+                        new SelectListItem { Text = OrganizationRoleEnum.Owner.ToString(), Value = "2" }
+                    },
+                    "Value",
+                    "Text",
+                    "1")
+            };
 
-			return View(model);
-		}
+            return View(model);
+        }
 
-		private static List<SelectListItem> GetSubRoles(SkuIdEnum skuId)
-		{
-			switch (skuId)
-			{
-				case SkuIdEnum.TimeTrackerBasic:
-					return new List<SelectListItem>
-					{
-						new SelectListItem { Text = Strings.Unassigned, Value = "0"},
-						new SelectListItem { Text = Strings.User, Value = "1"},
-						new SelectListItem { Text = Strings.Manager, Value = "2"}
-					};
-				case SkuIdEnum.ExpenseTrackerBasic:
-					return new List<SelectListItem>
-					{
-						new SelectListItem { Text = Strings.Unassigned, Value = "0"},
-						new SelectListItem { Text = Strings.User, Value = "1"},
-						new SelectListItem { Text = Strings.Manager, Value = "2"},
-						new SelectListItem { Text = Strings.SuperUser, Value = "4"},
-					};
-				case SkuIdEnum.StaffingManagerBasic:
-					return new List<SelectListItem>
-					{
-						new SelectListItem { Text = Strings.Unassigned, Value = "0"},
-						new SelectListItem { Text = Strings.User, Value = "1"},
-						new SelectListItem { Text = Strings.Manager, Value = "2"}
-					};
-			}
-			return null;
-		}
+        private static List<SelectListItem> GetSubRoles(SkuIdEnum skuId)
+        {
+            switch (skuId)
+            {
+                case SkuIdEnum.TimeTrackerBasic:
+                    return new List<SelectListItem>
+                    {
+                        new SelectListItem { Text = Strings.Unassigned, Value = "0"},
+                        new SelectListItem { Text = Strings.User, Value = "1"},
+                        new SelectListItem { Text = Strings.Manager, Value = "2"}
+                    };
 
-		/// <summary>
-		/// POST: /Add
-		/// Adding a new member to an organization.
-		/// </summary>
-		/// <param name="model">The View Model of user info passed from Add.cshtml.</param>
-		/// <returns>The result of this action.</returns>
-		[HttpPost]
-		[ValidateAntiForgeryToken]
-		public async Task<ActionResult> AddMember(AddMemberViewModel model)
-		{
-			if (!ModelState.IsValid) return View(model); // Invalid model, try again
+                case SkuIdEnum.ExpenseTrackerBasic:
+                    return new List<SelectListItem>
+                    {
+                        new SelectListItem { Text = Strings.Unassigned, Value = "0"},
+                        new SelectListItem { Text = Strings.User, Value = "1"},
+                        new SelectListItem { Text = Strings.Manager, Value = "2"},
+                        new SelectListItem { Text = Strings.SuperUser, Value = "4"},
+                    };
 
-			try
-			{
-				User usr = await AppService.GetUserByEmail(model.Email);
-				string url = usr != null ?
-					Url.Action(ActionConstants.Index, ControllerConstants.Account, null, Request.Url.Scheme) :
-					Url.Action(ActionConstants.Register, ControllerConstants.Account, null, Request.Url.Scheme);
+                case SkuIdEnum.StaffingManagerBasic:
+                    return new List<SelectListItem>
+                    {
+                        new SelectListItem { Text = Strings.Unassigned, Value = "0"},
+                        new SelectListItem { Text = Strings.User, Value = "1"},
+                        new SelectListItem { Text = Strings.Manager, Value = "2"}
+                    };
+            }
+            return null;
+        }
 
-				//string prodJson = "{{ "; //string.Format("{{ \"" + (int)ProductIdEnum.TimeTracker + "\" : {0}, \"" + (int)ProductIdEnum.ExpenseTracker + "\" : {1}, \"" + (int)ProductIdEnum.StaffingManager + "\" : 0 }}", model.ttSelection, model.etSelection);
+        /// <summary>
+        /// POST: /Add
+        /// Adding a new member to an organization.
+        /// </summary>
+        /// <param name="model">The View Model of user info passed from Add.cshtml.</param>
+        /// <returns>The result of this action.</returns>
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<ActionResult> AddMember(AddMemberViewModel model)
+        {
+            if (!ModelState.IsValid) return View(model); // Invalid model, try again
 
-				//foreach (var role in model.SubscriptionRoles)
-				//{
-				//	prodJson += "\"" + role.ProductId + "\" : " + role.SelectedRoleId + ", ";
-				//}
-				//prodJson = prodJson.TrimEnd(new char[] { ' ', ',' });
-				//prodJson += " }}";
+            try
+            {
+                User usr = await AppService.GetUserByEmail(model.Email);
+                string url = usr != null ?
+                    Url.Action(ActionConstants.Index, ControllerConstants.Account, null, Request.Url.Scheme) :
+                    Url.Action(ActionConstants.Register, ControllerConstants.Account, null, Request.Url.Scheme);
 
-				var json = model.SubscriptionRoles.Select(role => new InvitationPermissionsJson
-				{
-					ProductId = role.ProductId,
-					ProductRoleId = role.SelectedRoleId
-				})
-					.ToList();
+                //string prodJson = "{{ "; //string.Format("{{ \"" + (int)ProductIdEnum.TimeTracker + "\" : {0}, \"" + (int)ProductIdEnum.ExpenseTracker + "\" : {1}, \"" + (int)ProductIdEnum.StaffingManager + "\" : 0 }}", model.ttSelection, model.etSelection);
 
-				string jsonString = Newtonsoft.Json.JsonConvert.SerializeObject(json);
+                //foreach (var role in model.SubscriptionRoles)
+                //{
+                //	prodJson += "\"" + role.ProductId + "\" : " + role.SelectedRoleId + ", ";
+                //}
+                //prodJson = prodJson.TrimEnd(new char[] { ' ', ',' });
+                //prodJson += " }}";
 
-				int invitationId = await AppService.InviteUser(
-					url,
-					model.Email.Trim(),
-					model.FirstName,
-					model.LastName,
-					model.OrganizationId,
-					model.OrgRoleSelection == 2 ? OrganizationRoleEnum.Owner : OrganizationRoleEnum.Member,
-					model.EmployeeId,
-					jsonString);
+                var json = model.SubscriptionRoles.Select(role => new InvitationPermissionsJson
+                {
+                    ProductId = role.ProductId,
+                    ProductRoleId = role.SelectedRoleId
+                })
+                    .ToList();
 
-				Notifications.Add(new BootstrapAlert(string.Format(Strings.UserEmailed, model.FirstName, model.LastName), Variety.Success));
-				return RedirectToAction(ActionConstants.OrganizationMembers, new { id = model.OrganizationId });
-			}
-			catch (InvalidOperationException)
-			{
-				Notifications.Add(new BootstrapAlert(Strings.EmployeeIdNotUniqueError, Variety.Danger));
-				return View(model);
-			}
-			catch (System.Data.DuplicateNameException)
-			{
-				Notifications.Add(new BootstrapAlert(string.Format(Strings.UserAlreadyExists, model.FirstName, model.LastName), Variety.Warning));
-				return View(model);
-			}
-		}
-	}
+                string jsonString = Newtonsoft.Json.JsonConvert.SerializeObject(json);
+                string orgName = AppService.UserContext.OrganizationsAndRoles[model.OrganizationId].OrganizationName;
+
+                int invitationId = await AppService.InviteUser(
+                    url,
+                    model.Email.Trim(),
+                    model.FirstName,
+                    model.LastName,
+                    model.OrganizationId,
+                    orgName,
+                    model.OrgRoleSelection == 2 ? OrganizationRoleEnum.Owner : OrganizationRoleEnum.Member,
+                    model.EmployeeId,
+                    jsonString);
+
+                Notifications.Add(new BootstrapAlert(string.Format(Strings.UserEmailed, model.FirstName, model.LastName), Variety.Success));
+                return RedirectToAction(ActionConstants.OrganizationMembers, new { id = model.OrganizationId });
+            }
+            catch (InvalidOperationException)
+            {
+                Notifications.Add(new BootstrapAlert(Strings.EmployeeIdNotUniqueError, Variety.Danger));
+                return View(model);
+            }
+            catch (System.Data.DuplicateNameException)
+            {
+                Notifications.Add(new BootstrapAlert(string.Format(Strings.UserAlreadyExists, model.FirstName, model.LastName), Variety.Warning));
+                return View(model);
+            }
+        }
+    }
 }
