c616635f066c9a0db31831c394da4ea6a670c380
Luke Selbeck <luke.selbeck@allyis.com>
2017-11-21 15:39:10 -0800

    Re-add ProductRole, whoops

---
+++ AllyisAppsDB/Auth/Tables/ProductRole.sql
@@ -0,0 +1,12 @@
+﻿CREATE TABLE [Auth].[ProductRole] (
+	[ProductRoleId] INT           NOT NULL,
+	[ProductId]     INT           NOT NULL,
+	[ProductRoleName]          NVARCHAR (64) NOT NULL,
+	[OrganizationId] INT NULL, 
+	CONSTRAINT [PK_ProductRole] PRIMARY KEY CLUSTERED ([ProductRoleId] ASC, [ProductId] ASC),
+	CONSTRAINT [FK_ProductRole_Product] FOREIGN KEY ([ProductId]) REFERENCES [Billing].[Product] ([ProductId]),
+	CONSTRAINT [FK_ProductRole_Organization] FOREIGN KEY ([OrganizationId]) REFERENCES [Auth].[Organization] ([OrganizationId])
+);
+
+GO
+
