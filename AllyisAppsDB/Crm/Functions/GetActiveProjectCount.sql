5529ab57823561a4507b0d35afc57fd93e249a38
Avery Pratt <avery.pratt@allyis.com>
2017-11-22 17:29:45 -0800

    added computed columns to customer table for project count and active project count

---
+++ AllyisAppsDB/Crm/Functions/GetActiveProjectCount.sql
@@ -0,0 +1,16 @@
+﻿CREATE FUNCTION [Crm].[GetActiveProjectCount]
+(
+	@customerId INT
+)
+RETURNS SMALLINT
+AS
+BEGIN
+	DECLARE @count SMALLINT
+
+	SELECT @count = COUNT(*)
+	FROM [Pjm].[Project]
+	WHERE [customerId] = @customerId
+	AND GETUTCDATE() <= [EndUtc]
+
+	RETURN @count
+END
