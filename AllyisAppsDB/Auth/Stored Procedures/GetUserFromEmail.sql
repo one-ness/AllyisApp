465faa2db9957259a0f2e541868570754a8158ce
shankar bharadwaj <shankarab@gmail.com>
2018-01-30 12:55:49 -0800

    modified getuserfromemail, added getuserorganizations procedures

--- AllyisAppsDB/Auth/Stored Procedures/GetUserFromEmail.sql
+++ AllyisAppsDB/Auth/Stored Procedures/GetUserFromEmail.sql
@@ -1,28 +1,8 @@
-CREATE PROCEDURE [Auth].[GetUserFromEmail]
+﻿CREATE PROCEDURE [Auth].[GetUserFromEmail]
 	@email NVARCHAR(384)
 AS
 BEGIN
 	SET NOCOUNT ON;
-	SELECT [UserId]
-		,[FirstName]
-		,[LastName]
-		,[DateOfBirth]
-		,[Address1] as 'Address'
-		,[City]
-		,[State].[StateName] AS 'State'
-		,[Country].[CountryName] AS 'Country'
-		,[PostalCode]
-		,[Email]
-		,[PhoneNumber]
-		,[PhoneExtension]
-		,[LastUsedSubscriptionId]
-		,[PasswordHash]
-		,[PreferredLanguageId]
-		,[User].[IsEmailConfirmed]
-	FROM [Auth].[User]
-	WITH (NOLOCK)
-	LEFT JOIN [Lookup].[Address]	WITH (NOLOCK) ON [Address].[AddressId] = [User].[AddressId]
-	LEFT JOIN [Lookup].[Country]	WITH (NOLOCK) ON [Country].[CountryCode] = [Address].[CountryCode]
-	LEFT JOIN [Lookup].[State]		WITH (NOLOCK) ON [State].[StateId] = [Address].[StateId]
-	WHERE [Email] = @email;
+	SELECT u.* FROM [Auth].[User] u with (nolock)
+	WHERE u.[Email] = @email;
 END
---
+++ AllyisAppsDB/Auth/Stored Procedures/GetUserOrganizations.sql
@@ -0,0 +1,10 @@
+﻿
+CREATE procedure [Auth].[GetUserOrganizations]
+	@userId int
+as
+begin
+	set nocount on
+	select o.* from Organization o with (nolock)
+	inner join OrganizationUser ou with (nolock) on ou.OrganizationId = o.OrganizationId
+	where ou.UserId = @userId and o.IsActive = 1
+end
