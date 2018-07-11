4968ee4df7b808db7e23c0e81b4f326913de54a8
one-ness <keeganfisher91@gmail.com>
2018-07-11 11:45:48 -0700

    Fixed the unsubscribe script functionality to include removal of StaffTracker Subscription

--- AllyisAppsDB/Billing/Stored Procedures/DeactivateSubscription.sql
+++ AllyisAppsDB/Billing/Stored Procedures/DeactivateSubscription.sql
@@ -5,9 +5,9 @@ BEGIN
 	SET NOCOUNT ON;
 	Begin Transaction
 	DELETE FROM SubscriptionUser WHERE [SubscriptionId] = @subscriptionId
+	declare @status INT = (select [DefaultApplicationStatusId] FROM [Staffing].[StaffingSettings] WHERE [SubscriptionId] = @subscriptionId)
+	DELETE FROM [Staffing].[StaffingSettings] WHERE [SubscriptionId] = @subscriptionId
+	DELETE FROM [Staffing].[ApplicationStatus] WHERE [ApplicationStatusId] = @status 
 	DELETE FROM Subscription WHERE [SubscriptionId] = @subscriptionId
-	--UPDATE [Billing].[Subscription]
-	--SET [SubscriptionStatus] = 0
-	--WHERE [SubscriptionId] = @subscriptionId
 	Commit Transaction
 END
